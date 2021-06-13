a6d2d9c74c7a2a219e0e018ac10a0a1dc31b5bab
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-06-13 13:28:29 +0200

    feat: include trivial svelte setup

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -1,7 +1,12 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+
 export default {
-    input: 'src/main.js',
-    output: {
-      file: 'bundle.js',
-      format: 'cjs'
-    }
-  };
+  input: "src/main.js",
+  output: {
+    file: "public/bundle.js",
+    format: "iife",
+    name: "main"
+  },
+  plugins: [svelte(), resolve({ browser: true })],
+};
