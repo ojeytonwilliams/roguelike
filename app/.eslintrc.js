538772ff132261eca52de1cb0fc74d0a91344547
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-06-13 21:00:14 +0200

    feat: add prettier

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     // no type-aware rules, yet, since they can't fully handle svelte
     // https://github.com/sveltejs/eslint-plugin-svelte3#installation-with-typescript
+    "prettier",
   ],
   settings: {
     "svelte3/typescript": true, // load TypeScript as peer dependency
