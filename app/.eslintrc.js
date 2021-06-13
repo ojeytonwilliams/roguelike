26807aa05ce1ef736727794899aa2118b8d26bbd
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-06-13 20:47:42 +0200

    feat: add eslint-plugin-svelte3
    Again VSCode needs configuring so that it properly understands .svelte
    files

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,13 +1,20 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "svelte3"],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
   ],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     // no type-aware rules, yet, since they can't fully handle svelte
     // https://github.com/sveltejs/eslint-plugin-svelte3#installation-with-typescript
   ],
+  settings: {
+    "svelte3/typescript": true, // load TypeScript as peer dependency
+  },
 };
