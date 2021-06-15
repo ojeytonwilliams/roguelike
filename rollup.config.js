c812cde0cfb309269199cb6ac702cf3c1726a93d
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-06-15 23:58:45 +0200

    feat: add commonjs plugin to handle require
    core-js (unfortunately) makes use of require, so we need the commonjs
    plugin so that rollup sees an import realises it has to fetch the source
    and bundles it.  It's very clever, though, if you stop using whatever
    core-js was polyfilling for you then rollup doesn't just drop that it
    drops the commonjs transform, too.

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import autoPreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import { babel } from "@rollup/plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
 
 export default {
   input: "app/src/main.ts",
@@ -15,10 +16,14 @@ export default {
     svelte({ preprocess: autoPreprocess() }),
     typescript({ tsconfig: "./app/tsconfig.json" }),
     resolve({ browser: true }),
+    commonjs(), // we need this because core-js polyfills are require'd
     babel({
       babelHelpers: "bundled",
       extensions: [".js", ".ts", ".mjs", ".html", ".svelte"], // slight overkill, we only need .ts and .svelte, but it's better to be cautious than to miss files
       exclude: ["node_modules/**"], // this might be overkill in the other direction, since we may accidentally include es5 packages.
+      // including node_modules in general incurs a large bundle overhead, but if you're serious
+      // about supporting older browsers that's necessary.  I should revisit this when it's more
+      // than a toy app. https://github.com/obahareth/are-you-es5 might help!
     }),
   ],
 };
