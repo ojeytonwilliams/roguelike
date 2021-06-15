525a412b8803fa731f1eb97cc2a2a119fc40001e
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-06-15 23:07:08 +0200

    chore: setup babel
    Make sure we target the right environment, pull in corejs where needed,
    transpile all the extensions we might use and don't bundle up every
    module we use.  In the interest of having a small bundle, we trust
    the dependencies we use... for now!

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
 import autoPreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
+import { babel } from "@rollup/plugin-babel";
 
 export default {
   input: "app/src/main.ts",
@@ -14,5 +15,10 @@ export default {
     svelte({ preprocess: autoPreprocess() }),
     typescript({ tsconfig: "./app/tsconfig.json" }),
     resolve({ browser: true }),
+    babel({
+      babelHelpers: "bundled",
+      extensions: [".js", ".ts", ".mjs", ".html", ".svelte"], // slight overkill, we only need .ts and .svelte, but it's better to be cautious than to miss files
+      exclude: ["node_modules/**"], // this might be overkill in the other direction, since we may accidentally include es5 packages.
+    }),
   ],
 };
